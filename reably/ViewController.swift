97ba445efefa906e49974e96518a09bb1ffeaccc
borisbolsh <bolsh.bo@yandex.ru>
2021-11-25 23:31:46 +0300

    first commit

---
+++ reably/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  reably
+//
+//  Created by Boris Bolshakov on 25.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
